82405fca348ab3be680601a9cfc21f5023e91791
Funmi7 <talabifunmilayo@gmail.com>
2019-10-30 09:32:22 +0100

    Created post request for adding a new post

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const postsRouter = require('./routers/postsRouter');
-const postCommentsRouter = require('./routers/postCommentsRouter');
 
 const server = express();
 
 server.use(express.json());
 
 server.use('/api/posts', postsRouter);
-server.use('/api/posts', postCommentsRouter);
+
 
 server.get('/', (req, res) => {
     res.send(`
--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -3,24 +3,71 @@ const postsRouter = express.Router();
 const db = require('../data/db');
 
 postsRouter.post('/', (req, res) => {
-    const postInfo = req.body;
+  const postInfo = req.body;
+  if (!postInfo.title || !postInfo.contents) {
+    res.status(400).json({
+      errorMessage: 'Please provide title and contents for the post.'
+    })
+  } else {
     db.insert(postInfo)
-    .then(post => {
-       res.status(201).json(post) 
-    })
-    .catch(error => {
-        res.status(500).json(error)
-    })
+      .then(post => {
+        res.status(201).json(post)
+      })
+      .catch(error => {
+        res.status(500).json({
+          error: "There was an error while saving the post to the database"
+        })
+      })
+  }
 })
 
 postsRouter.get('/', (req, res) => {
-    db.find()
+  db.find()
     .then(posts => {
-        res.status(200).json(posts)
+      res.status(200).json(posts)
     })
     .catch(error => {
-        error
+      error
     });
 });
+
+
+postsRouter.post('/:id/comments', (req, res) => {
+  const { id } = req.params;
+  const { text } = req.body;
+  db.findById(id)
+    .then(data => {
+      if(data.length === 0) {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist."
+        })
+      } else if (!text) {
+        res.status(400).json({
+          errorMessage: "Please provide text for the comment."
+        })
+      } else {
+        return db.insertComment(text)
+      }   
+    })
+    .then(comment => {
+      res.status(201).json(comment)
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "There was an error while saving the comment to the database"
+      })
+    })
+})
+
+postsRouter.get(':id/comments', (req, res) => {
+  const { id } = req.params;
+  db.findPostComments(id)
+    .then(post => {
+      res.status(200).json(post)
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+})
 
 module.exports = postsRouter;
